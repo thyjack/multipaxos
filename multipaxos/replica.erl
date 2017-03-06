b8f434562b9229f004eebd16788acde5883bcb74
Jack Liu <me@thejoker.io>
2017-03-06 00:24:01 +0000

    First impl attempt for replica

--- multipaxos/replica.erl
+++ multipaxos/replica.erl
@@ -8,31 +8,61 @@
 start(Database) ->
   receive
     {bind, Leaders} -> 
-       next(...)
+       next(Database, Leaders, 1, 1, [], maps:new())
   end.
 
-next(...) ->
+next(Database, Leaders, SlotIn, SlotOut, Requests, Scroll) ->
   receive
     {request, C} ->      % request from client
-      ...
+      Requests2 = [C | Requests],
+      propose_next(Database, Leaders, SlotIn, SlotOut, Requests2, Scroll);
     {decision, S, C} ->  % decision from commander
-      ... = decide (...)
-  end, % receive
+      case maps:find(Scroll, S) of
+        {ok, {proposal, C2}} -> 
+          Requests2 = [C2 | Requests];
+        error -> 
+          Requests2 = Requests
+      end,
 
-  ... = propose(...),
-  ...
+      Scroll2 = Scroll#{ S := {decision, C} },
+      SlotOut2 = update_slot_out(SlotOut, Scroll2),
+      perform(Database, SlotOut, SlotOut2, Scroll2),
+      propose_next(Database, Leaders, SlotIn, SlotOut2, Requests2, Scroll2)
+  end. % receive
 
-propose(...) ->
+update_slot_out(SlotOut, Scroll) ->
+  case maps:find(Scroll, SlotOut) of
+    error -> SlotOut;
+    {ok, {proposal, _}} -> SlotOut;
+    {ok, {decision, _}} -> update_slot_out(SlotOut + 1, Scroll)
+   end.
+
+propose_next(Database, Leaders, SlotIn, SlotOut, [], Scroll) ->
+  next(Database, Leaders, SlotIn, SlotOut, [], Scroll);
+propose_next(Database, Leaders, SlotIn, SlotOut, Requests, Scroll) ->
   WINDOW = 5,
-  ...
-   
-decide(...) ->
-  ...
-       perform(...),
-  ...
+  
+  case (SlotIn < SlotOut + WINDOW) of
+    false -> next(Database, Leaders, SlotIn, SlotOut, Requests, Scroll);
+    true -> 
+      case maps:find(Scroll, SlotIn) of
+        {ok, _} -> propose_next(Database, Leaders, SlotIn + 1, SlotOut, Requests, Scroll);
+        error ->
+          C = lists:min(Requests),
+          Requests2 = lists:delete(C, Requests),
+          Scroll2 = Scroll#{ SlotIn := {proposal, C} },
+          [Leader ! {propose, SlotIn, C} || Leader <- Leaders],
+          propose_next(Database, Leaders, SlotIn + 1, SlotOut, Requests2, Scroll2)
+      end
+  end.
 
-perform(...) ->
-  ...
+perform(_, Start, Start, _) -> stop;
+perform(Database, Start, End, Scroll) ->
+  case maps:find(Scroll, Start) of
+    {ok, {decision, {Client, Cid, Op}}} ->
       Database ! {execute, Op},
       Client ! {response, Cid, ok}
+  end,
+  
+  perform(Database, Start + 1, End, Scroll).
 
