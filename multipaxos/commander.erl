d61a2cf75f8154a87c123b63f6d74cca2347767f
Oliver Wheeler <oliver.kuruma@gmail.com>
2017-03-06 08:49:14 +0000

    wam

--- multipaxos/commander.erl
+++ multipaxos/commander.erl
@@ -2,21 +2,24 @@
 -export([start/4]).
 
 start(Leader,Acceptors,Replicas,{B,S,C}) ->
+    command(Leader, Acceptors, Replicas, {B,S,C}, Acceptors).
+
+command(Leader,Acceptors,Replicas,{B,S,C}, WaitFor) ->
     [Acceptor ! {p2a, self(), {B,S,C}} ||Acceptor <- Acceptors],
     receive
         {p2b, Acceptor, AccBallotNum} ->
             if AccBallotNum == B ->
-                NewWaitFor = sets:del_element(Acceptor, Acceptors),
+                NewWaitFor = sets:del_element(Acceptor, WaitFor),
                 WaitForAccSize = sets:size(NewWaitFor),
                 AcceptorsHalfSize = sets:size(Acceptors) / 2,
                 if  WaitForAccSize < AcceptorsHalfSize  ->
-                    [Replica ! {decision, S, C} || Replica <- Replicas];
+                    [Replica ! {decision, S, C} || Replica <- Replicas],
+                    exit(commander);
                 true ->
-                    ok
+                    command(Leader,Acceptors,Replicas,{B,S,C}, NewWaitFor)
                 end;
             true ->
-                Leader ! {preempted, AccBallotNum}
-            end
-                
-    end,
-    exit(commander).
+                Leader ! {preempted, AccBallotNum},
+                exit(commander)
+            end       
+    end.
--- multipaxos/scout.erl
+++ multipaxos/scout.erl
@@ -1,2 +1,21 @@
 -module(scout).
--export([start/0]).
+-export([start/3]).
+
+start(Leader, Acceptors, BallotNum) ->
+    scout(Leader, Acceptors, BallotNum, Acceptors).
+
+
+scout(Leader, Acceptors, BallotNum, WaitFor) ->
+    [Acceptor ! {p1a, self(), BallotNum} ||Acceptor <- Acceptors],
+    receive
+        {p1b, Acceptor, BallotAcc, Accepted} ->
+            if BallotAcc == BallotNum ->
+                NewWaitFor = sets:del_element(Acceptor, WaitFor),
+                WaitForAccSize = sets:size(NewWaitFor),
+                AcceptorsHalfSize = sets:size(Acceptors) / 2,
+                if  WaitForAccSize < AcceptorsHalfSize ->
+                    Leader ! {adopted, BallotNum, };
+                true ->
+                    ok
+                end;
+    end.
