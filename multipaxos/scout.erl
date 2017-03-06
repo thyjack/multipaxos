fa24e9b67eaeb3d4ddd37edf226240aa448251c6
Oliver  Wheeler <oliver.wheeler14@imperial.ac.uk>
2017-03-06 09:07:24 +0000

    scout compiles

--- multipaxos/scout.erl
+++ multipaxos/scout.erl
@@ -2,20 +2,26 @@
 -export([start/3]).
 
 start(Leader, Acceptors, BallotNum) ->
-    scout(Leader, Acceptors, BallotNum, Acceptors).
+    scout(Leader, Acceptors, BallotNum, Acceptors, sets:new()).
 
 
-scout(Leader, Acceptors, BallotNum, WaitFor) ->
-    [Acceptor ! {p1a, self(), BallotNum} ||Acceptor <- Acceptors],
+scout(Leader, Acceptors, BallotNum, WaitFor, PValues) ->
+    [Acceptor ! {p1a, self(), BallotNum} || Acceptor <- Acceptors],
     receive
         {p1b, Acceptor, BallotAcc, Accepted} ->
             if BallotAcc == BallotNum ->
+                NewPValues = sets:union(PValues, Accepted),
                 NewWaitFor = sets:del_element(Acceptor, WaitFor),
                 WaitForAccSize = sets:size(NewWaitFor),
                 AcceptorsHalfSize = sets:size(Acceptors) / 2,
                 if  WaitForAccSize < AcceptorsHalfSize ->
-                    Leader ! {adopted, BallotNum, };
+                    Leader ! {adopted, BallotNum, NewPValues},
+                    exit(scout);
                 true ->
-                    ok
+                    scout(Leader, Acceptors, BallotNum, NewWaitFor, NewPValues)
                 end;
+            true ->
+                Leader ! {preempted, BallotAcc},
+                exit(scout)
+            end
     end.
