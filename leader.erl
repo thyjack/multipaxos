e509210cda628926fe592d6921f6c7ae4568e92d
Oliver  Wheeler <oliver.wheeler14@imperial.ac.uk>
2017-03-06 10:01:49 +0000

    some method

--- leader.erl
+++ leader.erl
@@ -4,10 +4,21 @@
 start() ->
   receive
     {Acceptors, Replicas} ->
-      lead(Acceptors, Replicas, 0, false, sets:new())
+      lead(Acceptors, Replicas, 0)
   end.
 
-lead(Acceptors, _Replicas, BallotNum, _Active, _Proposals) ->
-  spawn(scout, start, [self(), Acceptors, BallotNum]).
+lead(Acceptors, Replicas, BallotNum) ->
+  spawn(scout, start, [self(), Acceptors, BallotNum]),
+  lead_loop(Acceptors, Replicas, 0, false, sets:new()).
 
-%lead_loop(Acceptors, Replicas, BallotNum, Active, Proposals) ->
+lead_loop(Acceptors, Replicas, BallotNum, Active, Proposals) ->
+  receive
+    {propose, S, C} ->
+      _ContainsCommand = contains_command(Proposals, C)
+  end.
+
+
+contains_command(Proposals, C) ->
+  ProposalList = sets:to_list(Proposals),
+  Commands = lists:unzip(ProposalList),
+  lists:member(C, Commands).
