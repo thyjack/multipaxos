66fa1f380e5ec12fc562b4f3a864aa8bed6c7ca7
Jack Liu <me@thejoker.io>
2017-03-08 14:16:57 +0000

    Code style fix

--- acceptor.erl
+++ acceptor.erl
@@ -2,25 +2,25 @@
 -export([start/0]).
 
 start() ->
-    accept(0, sets:new()).
+  accept(0, sets:new()).
 
 accept(BallotNum, Accepted) ->
   receive
-        {p1a, Leader, Ballot} ->      
-            if Ballot > BallotNum ->
-                Leader ! {p1b, self(), Ballot, Accepted},
-                accept(Ballot, Accepted);
-            true ->
-                Leader ! {p1b, self(), BallotNum, Accepted},
-                accept(BallotNum, Accepted)
-            end;
-        {p2a, Leader, {B, S, C}} ->
-            if B == BallotNum ->
-                NewSet = sets:add_element(Accepted, {B, S, C}),
-                Leader ! {p2b, self(), B, NewSet},
-                accept(B, NewSet);
-            true ->
-                Leader ! {p2b, self(), BallotNum},
-                accept(BallotNum, Accepted)
-            end
-    end.
+    {p1a, Leader, Ballot} ->      
+      if Ballot > BallotNum ->
+        Leader ! {p1b, self(), Ballot, Accepted},
+        accept(Ballot, Accepted);
+      true ->
+        Leader ! {p1b, self(), BallotNum, Accepted},
+        accept(BallotNum, Accepted)
+      end;
+    {p2a, Leader, {B, S, C}} ->
+      if B == BallotNum ->
+        NewSet = sets:add_element(Accepted, {B, S, C}),
+        Leader ! {p2b, self(), B, NewSet},
+        accept(B, NewSet);
+      true ->
+        Leader ! {p2b, self(), BallotNum},
+        accept(BallotNum, Accepted)
+      end
+  end.
--- scout.erl
+++ scout.erl
@@ -2,30 +2,31 @@
 -export([start/3]).
 
 start(Leader, Acceptors, BallotNum) ->
-    scout(Leader, Acceptors, BallotNum).
+  scout(Leader, Acceptors, BallotNum).
 
 
 scout(Leader, Acceptors, BallotNum) ->
-    [Acceptor ! {p1a, self(), BallotNum} || Acceptor <- Acceptors],
-    scout_loop(Leader, Acceptors, BallotNum, Acceptors, sets:new()).
+  [Acceptor ! {p1a, self(), BallotNum} || Acceptor <- Acceptors],
+  scout_loop(Leader, Acceptors, BallotNum, Acceptors, sets:new()).
 
 
 scout_loop(Leader, Acceptors, BallotNum, WaitFor, PValues) ->
   receive
-      {p1b, Acceptor, BallotAcc, Accepted} ->
-          if BallotAcc == BallotNum ->
-              NewPValues = sets:union(PValues, Accepted),
-              NewWaitFor = sets:del_element(Acceptor, WaitFor),
-              WaitForAccSize = sets:size(NewWaitFor),
-              AcceptorsHalfSize = sets:size(Acceptors) / 2,
-              if  WaitForAccSize < AcceptorsHalfSize ->
-                  Leader ! {adopted, BallotNum, NewPValues},
-                  exit(scout);
-              true ->
-                  scout_loop(Leader, Acceptors, BallotNum, NewWaitFor, NewPValues)
-              end;
+    {p1b, Acceptor, BallotAcc, Accepted} ->
+      if BallotAcc == BallotNum ->
+        NewPValues = sets:union(PValues, Accepted),
+        NewWaitFor = sets:del_element(Acceptor, WaitFor),
+        WaitForAccSize = sets:size(NewWaitFor),
+        AcceptorsHalfSize = sets:size(Acceptors) / 2,
+        if 
+          WaitForAccSize < AcceptorsHalfSize ->
+            Leader ! {adopted, BallotNum, NewPValues},
+            exit(scout);
           true ->
-              Leader ! {preempted, BallotAcc},
-              exit(scout)
-          end
+            scout_loop(Leader, Acceptors, BallotNum, NewWaitFor, NewPValues)
+        end;
+      true ->
+        Leader ! {preempted, BallotAcc},
+        exit(scout)
+      end
   end.
